cb6466236e444a1b92ddd9cd550f6af36a25f630
Evan Spector <espector19@ssdsnassau.org>
2018-10-08 14:33:35 -0400

    Geometric designs
    Using Java I designed 3-Dimensional shapes

---
+++ ch4/src/ch4/Practice.java
@@ -0,0 +1,15 @@
+package ch4;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//a program that calculates the area of a circle when the radius is 10.
+		//a program that calculates the area of a circle when the radius is 5.5.
+		
+		System.out.println(Math.PI*10*10);
+		System.out.println(Math.PI*5.5*5.5);
+
+	}
+
+}
